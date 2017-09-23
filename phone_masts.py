450e912632ac41c707e7321c8fc71472e0253dd5
tdj <tdj@deb64.home.edu>
2017-09-23 16:46:32 +0100

    Finds data where 'Lease Years' == 25

--- phone_masts.py
+++ phone_masts.py
@@ -6,24 +6,35 @@ with open('Mobile Phone Masts.csv', newline='') as csvfile:
     # Sort the data - by 'Current Rent' ascending
     sorted_by_rent = sorted(reader, key=lambda row: float(row['Current Rent']))
 # Output the data to console
+print("\n\nThe first 5 entries in Mobile Phone Masts.csv, ", end="")
+print("sorted by 'Current Rent' (ascending), are: \n")
+
 for data in sorted_by_rent[:5]:
     print(data)
 
-# Tests to check the data being returned
+# Tests to check that the correct data is being returned
 assert(sorted_by_rent[0]['Unit Name'] == 'Potternewton Est Playing Field')
 assert(sorted_by_rent[1]['Unit Name'] == 'Queenswood Hgt-Telecom App.')
 assert(sorted_by_rent[2]['Unit Name'] == 'Burnsall Grange CSR 37865')
 assert(sorted_by_rent[3]['Unit Name'] == 'Seacroft Gate (Chase) block 2-Telecom App.')
 assert(sorted_by_rent[4]['Unit Name'] == 'Seacroft Gate (Chase) - Block 2, WYK 0414')
 
-
-
-
-
 # Create list where 'Lease Years' == 25
+lease_years_25 = []
+print("\n\nThe data entries where 'Lease Years' == 25 are:\n")
+for row in sorted_by_rent:
+    if int(row['Lease Years']) == 25:
+        lease_years_25.append(row)
+        # Print the list to the console
+        print(row)
 
+# Tests to check that the correct data is being returned
+assert(lease_years_25[0]['Unit Name'] == 'Queenswood Hgt-Telecom App.')
+assert(lease_years_25[1]['Unit Name'] == 'Burnsall Grange CSR 37865')
+assert(lease_years_25[2]['Unit Name'] == 'Seacroft Gate (Chase) block 2-Telecom App.')
+assert(lease_years_25[3]['Unit Name'] == 'Seacroft Gate (Chase) - Block 2, WYK 0414')
+assert(len(lease_years_25) == 4)
 
-# Print the list to the console
 
 
 # Calculate the total rent for the list (where 'Lease Years' == 25)
