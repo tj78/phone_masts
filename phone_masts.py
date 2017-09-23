f1fd83c8012bc7d0c58d3a326ad5bcca2a3f3f94
tj78 <timjealous@gmail.com>
2017-09-23 15:44:36 +0100

    First commit of phone_masts.py
    Outlined the task in pseudocode

---
+++ phone_masts.py
@@ -0,0 +1,33 @@
+# Read in the CSV file
+
+
+
+# Sort the data - by 'Current Rent' ascending
+
+
+# Output the data to console
+
+
+# Create list where 'Lease Years' == 25
+
+
+# Print the list to the console
+
+
+# Calculate the total rent for the list (where 'Lease Years' == 25)
+
+
+# Print the total to the console
+
+
+# Create a dictionary with a count of each tenant's masts, i.e.
+#   {'Tenant': mast_count, etc.}
+
+
+# Print this dictionary in a readable format
+
+
+# List the data where 'Lease Start Date' is between 1st June 1999 and 31st August 2007
+
+
+# Print this data, with the date in the format 'DD/MM/YYYY'
